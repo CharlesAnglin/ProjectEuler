14bceaccb8b19426babf3ef69261c2154c3a9698
CharlesAnglin <charlie.anglin@gmail.com>
2016-09-18 19:59:10 +0100

    adding solution to problem 4

--- src/ProjectEuler.scala
+++ src/ProjectEuler.scala
@@ -2,7 +2,12 @@
 
 object ProjectEuler {
   def main(args: Array[String]) {
-    val problem = new pe003
+    val start = System.currentTimeMillis()
+
+    val problem = new pe004
     problem.solve
+
+    val totalTime = System.currentTimeMillis - start
+    println("Elapsed time: %1d sec".format(totalTime/1000))  //rounded to lowest sec.
   }
 }
---
+++ src/pe004.scala
@@ -0,0 +1,41 @@
+class pe004 {
+
+  def solve = {
+    println(findLargestPalindrome)
+  }
+
+  def isPalindrome(number: String):Boolean = {
+    val length = number.length
+    length match {
+      case 1 => true
+      case 2 => if(number(0)==number(1)) true else false
+      case _ => if(number(0)==number(length-1)) isPalindrome(number.slice(1,length-1)) else false
+    }
+  }
+
+  def findLargestPalindrome = {
+    def helper(nextNumber: Int): Int = {
+      if(isPalindrome(nextNumber.toString)==true && isThreeDigitProduct(nextNumber)){
+        nextNumber
+      }
+      else{
+        helper(nextNumber-1)
+      }
+    }
+
+    helper(999*999)
+  }
+
+  def isThreeDigitProduct(number: Int, divisor: Int = 999) : Boolean = {
+    if(divisor == 99){
+      return false
+    }
+    if(number%divisor==0 && (number/divisor).toString.length==3){
+      true
+    } else {
+      isThreeDigitProduct(number, divisor-1)
+    }
+  }
+
+
+}
