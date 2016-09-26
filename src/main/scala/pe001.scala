5504e4404fc19a79bba16b703d19f80c3cac7135
CharlesAnglin <charlie.anglin@gmail.com>
2016-09-26 21:24:52 +0100

    refactoring problem 1

--- src/main/scala/pe001.scala
+++ src/main/scala/pe001.scala
@@ -2,9 +2,17 @@ import scala.collection.mutable.ArrayBuffer
 
 class pe001 {
 
-//  Elapsed time: 258 ms
+//  Elapsed time: 190 ms
 
-  def solve = {
+  def solve = (0 to 1000).filter(a => a % 3 == 0 || a % 5 == 0).sum
+
+  def solve2 = {
+    val numbers = List.range(0, 1000)
+    val filter = numbers.filter(a => a % 3 == 0 || a % 5 == 0)
+    println(filter.sum)
+  }
+
+  def solve3 = {
     val numbers = new ArrayBuffer[Int]
     for (num <- 0 to 999) {
       if (num % 5 == 0 || num % 3 == 0) {
@@ -13,11 +21,4 @@ class pe001 {
     }
     println(numbers.sum)
   }
-
-  def solve2 = {
-    val numbers = List.range(0, 1000)
-    val filter = numbers.filter(a => a % 3 == 0 || a % 5 == 0)
-    println(filter.sum)
-  }
-
 }
